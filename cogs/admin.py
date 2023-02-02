4e4e18db221836eda1c9587ac558dcd85db18826
Monikaya <64984313+Monikaya@users.noreply.github.com>
2023-02-01 20:49:56 -0800

    made admin commands

---
+++ cogs/admin.py
@@ -0,0 +1,51 @@
+import discord
+
+from discord.ext import commands
+
+class admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def purge(self, ctx, amount):
+        await ctx.message.delete()
+        
+        try:
+            await ctx.channel.purge(limit=int(amount))
+            await ctx.send(f"Deleted {amount} messages", delete_after=5)
+        except Exception:
+            pass
+
+    @commands.command()
+    async def clean(self,ctx, amount):
+        print("NOT IMPLEMENTED YET :D")
+
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await ctx.message.delete()
+        await member.kick(reason=reason)
+        await ctx.send(f"{member} has been kicked", delete_after=5)
+    
+    @commands.command()
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await ctx.message.delete()
+        await member.ban(reason=reason)
+        await ctx.send(f"{member} has been banned", delete_after=5)
+    
+    @commands.command()
+    async def unban(self, ctx, *, member):
+        await ctx.message.delete()
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"{user.mention} has been unbanned", delete_after=5)
+                return
+
+def setup(bot):
+    bot.add_cog(admin(bot))
+    print("utils loaded successfully")
